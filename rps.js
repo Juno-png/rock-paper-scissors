94bb07bead1350b708754833a70837f94cd18709
Juno <doplitog@gmail.com>
2022-11-17 00:53:12 -0800

    Make game play one round and return the outcome
    Rock, Paper, Scissors game now plays one round and
    sucessfully logs the outcome of the battle.
    
    Changes to be committed:
    	modified:   index.html
    	modified:   rps.js

--- rps.js
+++ rps.js
@@ -1,10 +1,13 @@
+let playerSelection = getPlayerChoice();
+let computerSelection = getComputerChoice();
+
 
 // get computer to randomly pick rock(0), paper(1), or scissors(2)
 function getComputerChoice () {
-    let randomNumber = Math.floor(Math.random() * 3)
+   let randomNumber = Math.floor(Math.random() * 3)
     // make computer print the outcome of its choice
-    switch (randomNumber) {
-        case 0:
+   switch (randomNumber) {
+       case 0:
             return 'rock'
         case 1:
             return 'paper'
@@ -12,17 +15,29 @@ function getComputerChoice () {
             return 'scissors'
     }
 }
-console.log(getComputerChoice());
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return 'tie game!'
-    } else {
-        return 'not a tie game'
-    }
-        
+console.log('NPC chose: ', computerSelection);
+
+function getPlayerChoice () {
+    let playerchoice = prompt('Choose your weapon')
+    return playerchoice.toLowerCase()
 }
 
-const playerSelection = 'rock'
-const computerSelection = getComputerChoice();
+console.log('player chose: ', playerSelection);
+
+// play one round and declare winner or tie
+function playRound(playerSelection, computerSelection) {
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+
+        return 'You win! ' + playerSelection + ' beats ' + computerSelection
+    } else if (playerSelection === computerSelection) {
+        return 'tie'
+    } else {
+        return 'You lost! ' + computerSelection + ' beats ' + playerSelection
+    }
+}
+
 console.log(playRound(playerSelection, computerSelection));
