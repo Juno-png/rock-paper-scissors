a1a994aebf21861a3b3995d069d923d0e33528d1
Juno <doplitog@gmail.com>
2022-12-08 16:53:24 -0800

    Finish rps UI and complete styling
    Added the DOM methods that changed the previous console.logs
    for score and player/computer choices.
    Also added more styling.

--- rps.js
+++ rps.js
@@ -1,92 +1,57 @@
 let playerScore = 0
-let npcScore = 0
-let draws = 0
-
-function resetGame() {
-    //reset game
-}
-
-function startGame() {
-// play the game until someone wins 5 times
-    playRound();
-
-    setWins();
-}
+let computerScore = 0
+const buttons = document.querySelectorAll('button')
 
 
 // function that randomly returns computer choice
-// TODO update the DOM with computerSelection
+
 function getComputerChoice () {
-    let random = Math.floor(Math.random() * 3);
-    switch (random) {
-        case 0:
-            return 'rock'
-        case 1:
-            return 'paper'
-        case 2:
-            return 'scissors'
-    }
-   
+    let choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * 3)];
 }
 
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
 
 // function that plays one round of rps, two parameters player choice, and computer choice
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return playerWin + 'rock beats scissors'
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return playerWin + 'paper beats rock'
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return playerWin + 'scissors beats paper'
-    } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
-        return npcWin + 'rock beats scissors'
-    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
-        return npcWin + 'paper beats rock'
-    } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-        return npcWin + 'scissors beats paper'
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    let result;
+  
+    if (
+      (playerSelection === 'rock' && computerSelection === 'scissors') ||
+      (playerSelection === 'scissors' && computerSelection === 'paper') ||
+      (playerSelection === 'paper' && computerSelection === 'rock')
+    ) {
+      playerScore += 1;
+      result = `You win! ${playerSelection} beats ${computerSelection}<br><br>Player score: ${playerScore}<br>Computer score: ${computerScore}`;
+  
+      if (playerScore === 5) {
+        result += `<br><br>You won the game! Reload the page to play again`;
+        disableButtons();
+      }
+    } else if (playerSelection === computerSelection) {
+      result = `It's a tie. You both chose ${playerSelection}<br><br>Player score: ${playerScore}<br>Computer score: ${computerScore}`;
     } else {
-        return draw
-    }     
-}
-
-let draw = 'Draw! no points!'
-let playerWin = 'You win!'
-let npcWin = 'You lose!'
-
-// function that calls playRound to play a 5 round game that keeps score and outputs outcome
-
-//for (let i = 0; i < 5; i++) {
-    //let playerSelection = window.prompt('rock,paper, or scissors?').toLowerCase();
-
-    const computerSelection = getComputerChoice();
-    let roundResult = playRound(playerSelection, computerSelection);
-    game(roundResult);
-//}
-
-function checkWinner (result) {
-    if ((result === playerWin + 'rock beats scissors') ||
-        (result === playerWin + 'paper beats rock') ||
-        (result === playerWin + 'scissors beats paper')) {
-        playerScore++;
-    } else if  ((result === npcWin + 'rock beats scissors') || 
-                (result === npcWin + 'paper beats rock') || 
-                (result === npcWin + 'scissors beats paper')) {
-            npcScore++;
-    } else {
-        draws++;
-    }    
-}
-
-(gameover());
-
-function gameover() {
-    if (playerScore === npcScore) {
-    ('Draw! No winner');
-    } else if (npcScore > playerScore) {
-    ('NPC WINS! you suck (<.<)');
-    } else {
-    ('PLAYER WINS! (^.^)');
+      computerScore += 1;
+      result = `You lose! ${computerSelection} beats ${playerSelection}<br><br>Player score: ${playerScore}<br>Computer score: ${computerScore}`;
+  
+      if (computerScore === 5) {
+        result += `<br><br>Computer wins the game! You're Trash! Reload the page to play again`;
+        disableButtons();
+      }
     }
-    return 'GAMEOVER THANKS FOR PLAYING!'
-}
+  
+    document.getElementById('result').innerHTML = result;
+  }
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        playRound(button.id)
+    })
+})
